939e7bc96b9e7f6a59201253b0ccd94b592471de
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-20 12:34:06 +0530

    Event-import

--- app/components/events/event-import-section.js
+++ app/components/events/event-import-section.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import FormMixin from 'open-event-frontend/mixins/form';
 
-const { Component } = Ember;
+const { Component, run } = Ember;
 
 export default Component.extend(FormMixin, {
   classNames: ['ui', 'stackable', 'centered', 'grid'],
@@ -28,9 +28,51 @@ export default Component.extend(FormMixin, {
       }
     };
   },
+  eventDownloadUrl   : '',
+  eventExportStatus  : 'Event export not yet started.',
+  isDownloadDisabled : true,
+  requestLoop(exportJobInfo) {
+    run.later(() => {
+      this.get('loader')
+        .load(exportJobInfo.task_url, { withoutPrefix: true })
+        .then(exportJobStatus => {
+          if (exportJobStatus.state === 'SUCCESS') {
+            this.set('isLoading', false);
+            this.set('isDownloadDisabled', false);
+            this.set('eventDownloadUrl', exportJobStatus.result.download_url);
+            this.set('eventExportStatus', exportJobStatus.state);
+            this.get('notify').success(this.l10n.t('Event exported.'));
+          } else if (exportJobStatus.state === 'WAITING') {
+            this.requestLoop(exportJobInfo);
+            this.set('eventExportStatus', exportJobStatus.state);
+            this.get('notify').alert(this.l10n.t('Event export is going on.'));
+          } else {
+            this.set('isLoading', false);
+            this.set('eventExportStatus', exportJobStatus.state);
+            this.get('notify').error(this.l10n.t('Event export failed.'));
+          }
+        })
+        .catch(() => {
+          this.set('isLoading', false);
+          this.set('eventExportStatus', 'FAILURE');
+          this.get('notify').error(this.l10n.t('Event export failed.'));
+        });
+    }, 3000);
+  },
+
   actions: {
     submit() {
       this.onValid(() => {
+        this.set('isLoading', true);
+        let payload = this.get('data');
+        this.get('loader')
+          .post(`/events/import/json`, payload)
+          .then(exportJobInfo => {
+            this.requestLoop(exportJobInfo);
+          })
+          .catch(() => {
+            this.get('notify').error(this.l10n.t('Unexpected error occurred.'));
+          });
       });
     }
   }
